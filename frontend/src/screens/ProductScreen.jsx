44ea7ce10f7c4be8eb39f5f9174b8db3ec24afc0
Junaid <chjunaid89@gmail.com>
2025-08-21 11:54:55 -0700

    frontend-react, homepage and product page, backend-django, serving & fetching data with DRF, completed

---
+++ frontend/src/screens/ProductScreen.jsx
@@ -0,0 +1,99 @@
+import React, { useState, useEffect } from "react";
+import { Link, useParams } from "react-router-dom";
+import { Image, Row, Col, ListGroup, Card, Button } from "react-bootstrap";
+// import products from "../products";
+import Rating from "../components/Rating";
+import axios from "axios";
+
+function ProductScreen() {
+  const [product, setProduct] = useState([]);
+  const { id } = useParams();
+
+  useEffect(() => {
+    async function fetchProduct() {
+      const { data } = await axios.get(`/api/products/${id}`);
+      setProduct(data);
+    }
+
+    fetchProduct();
+  }, []);
+
+  // const { id } = useParams();
+  // const product = products.find((p) => p._id === id);
+
+  return (
+    <div>
+      <Link to="/" className="btn btn-light my-3">
+        Go Back
+      </Link>
+      <Row>
+        <Col md={6}>
+          <Image src={product.image} alt={product.name} fluid />
+        </Col>
+
+        <Col md={3}>
+          <ListGroup>
+            <ListGroup.Item>
+              <h2>{product.name}</h2>
+            </ListGroup.Item>
+
+            <ListGroup.Item>
+              <Rating
+                value={product.rating}
+                text={`${product.numReviews} reviews`}
+                color={"#f8e825"}
+              />
+            </ListGroup.Item>
+
+            <ListGroup.Item>
+              <h5>Price</h5>
+              <h3>${product.price}</h3>
+            </ListGroup.Item>
+
+            <ListGroup.Item>
+              <h5>Description</h5>
+              {product.description}
+            </ListGroup.Item>
+          </ListGroup>
+        </Col>
+
+        <Col md={3}>
+          <Card className="my-1 p-1 rounded border border-secondary">
+            <ListGroup>
+              <ListGroup.Item>
+                <Row>
+                  <Col>Price:</Col>
+                  <Col>
+                    <strong>{product.price}</strong>
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+
+              <ListGroup.Item>
+                <Row>
+                  <Col>Status:</Col>
+                  <Col>
+                    {product.countInStock > 0 ? "Instock" : "Out of Stock"}
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+
+              <ListGroup.Item>
+                <Button
+                  variant="primary"
+                  className="w-100"
+                  type="button"
+                  disabled={product.countInStock == 0}
+                >
+                  Add to Cart
+                </Button>
+              </ListGroup.Item>
+            </ListGroup>
+          </Card>
+        </Col>
+      </Row>
+    </div>
+  );
+}
+
+export default ProductScreen;
