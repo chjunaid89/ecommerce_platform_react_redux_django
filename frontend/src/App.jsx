d1451516f9f60401de6ee92171790a89b2d22d64
Junaid <chjunaid89@gmail.com>
2025-10-02 07:23:12 -0700

    feat(order): implement order details flow with backend + frontend integration
    - Added Django view to fetch order by ID
    - serialized with order details serializer with nested data
    - Implemented Redux reducer and action to manage order details state
    - Built OrderScreen to display:
      • Shipping details
      • User details
      • Order items
      • Payment info
      • Paid / delivered status indicators
      • Order Summary

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,6 +12,7 @@ import ProfileScreen from "./screens/ProfileScreen";
 import ShippingScreen from "./screens/ShippingScreen";
 import PaymentScreen from "./screens/PaymentScreen";
 import PlaceOrderScreen from "./screens/PlaceOrderScreen";
+import OrderScreen from "./screens/OrderScreen";
 
 function App() {
   return (
@@ -29,6 +30,7 @@ function App() {
             <Route path="/shipping" Component={ShippingScreen} />
             <Route path="/payment" Component={PaymentScreen} />
             <Route path="/placeorder" Component={PlaceOrderScreen} />
+            <Route path="/order/:id" Component={OrderScreen} />
           </Routes>
         </Container>
       </main>
